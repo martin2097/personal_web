7e0780ef7751773f62a9bb477ccd94bea0c51ebb
martin2097 <rapavy.mato@gmail.com>
2023-04-12 00:26:59 +0200

    update groupovacieho algo

--- pages/group-creator.py
+++ pages/group-creator.py
@@ -5,6 +5,7 @@ import itertools
 from datetime import datetime
 from time import time
 import math
+import copy
 
 dash.register_page(__name__)
 
@@ -198,6 +199,157 @@ def list_from_input(lst):
     return out_lst
 
 
+def one_by_one_solution(lst, maxsum):
+    ordered_unique_lst = list(set(lst))
+    print(ordered_unique_lst)
+    ordered_unique_lst.sort(reverse=True)
+    comb = []
+    for val in ordered_unique_lst:
+        for _ in range(lst.count(val)):
+            added = 0
+            i = 0
+            for c in comb:
+                if maxsum-sum(c) >= val:
+                    comb[i] += [val]
+                    added = 1
+                    break
+                i += 1
+            if added == 0:
+                comb.append([val])
+    return comb
+
+
+def get_remot(lst_of_lst):
+    unique_list = list(set([item for sublist in lst_of_lst for item in sublist]))
+    tot_rem = 0
+    for val in unique_list:
+        min_ind = 1000000
+        max_ind = 0
+        pos = 0
+        for i in lst_of_lst:
+            if val in i:
+                if pos < min_ind:
+                    min_ind = pos
+                if pos > max_ind:
+                    max_ind = pos
+            pos += 1
+        tot_rem += max_ind - min_ind
+    return tot_rem
+
+
+def best_one_by_one_solution(lst, maxsum):
+    ordered_unique_lst = list(set(lst))
+    best_sol = []
+    shortest_len = 10000
+    smallest_sums = 1000000
+    for order in itertools.permutations(ordered_unique_lst):
+        comb = []
+        for val in order:
+            for _ in range(lst.count(val)):
+                added = 0
+                i = 0
+                for c in comb:
+                    if maxsum-sum(c) >= val:
+                        comb[i] += [val]
+                        added = 1
+                        break
+                    i += 1
+                if added == 0:
+                    comb.append([val])
+        if len(comb) < shortest_len:
+            best_sol = comb
+            shortest_len = len(comb)
+            smallest_sums = get_remot(comb)
+        elif len(comb) == shortest_len:
+            if get_remot(comb) < smallest_sums:
+                smallest_sums = get_remot(comb)
+                best_sol = comb
+    return best_sol
+
+
+def one_by_one_with_splitting(lst, maxsum):
+    result = []
+    half_list = []
+    for i in set(lst):
+        cnt = lst.count(i)
+        half_list += ([i] * math.floor(cnt / 2))
+    solution = one_by_one_solution(half_list, maxsum)
+    rest_of_list = lst.copy()
+    for i in solution:
+        result.append(i.copy())
+        result.append(i.copy())
+        for j in i:
+            rest_of_list.remove(j)
+            rest_of_list.remove(j)
+    for r in rest_of_list:
+        added = 0
+        i = 0
+        for c in result:
+            if maxsum - sum(c) >= r:
+                result[i] += [r]
+                added = 1
+                break
+            i += 1
+        if added == 0:
+            result.append([r])
+    return result
+
+
+def best_one_by_one_with_splitting(lst, maxsum):
+    result = []
+    half_list = []
+    for i in set(lst):
+        cnt = lst.count(i)
+        half_list += ([i] * math.floor(cnt / 2))
+    solution = best_one_by_one_solution(half_list, maxsum)
+    rest_of_list = lst.copy()
+    for i in solution:
+        result.append(i.copy())
+        result.append(i.copy())
+        for j in i:
+            rest_of_list.remove(j)
+            rest_of_list.remove(j)
+    best_sol = []
+    shortest_len = 10000
+    smallest_sums = 1000000
+    for order in itertools.permutations(list(set(rest_of_list))):
+        comb = copy.deepcopy(result)
+        for val in order:
+            for _ in range(rest_of_list.count(val)):
+                added = 0
+                i = 0
+                for c in comb:
+                    if maxsum - sum(c) >= val:
+                        comb[i] += [val]
+                        added = 1
+                        break
+                    i += 1
+                if added == 0:
+                    comb.append([val])
+        if len(comb) < shortest_len:
+            best_sol = comb.copy()
+            shortest_len = len(comb)
+            smallest_sums = get_remot(comb)
+        elif len(comb) == shortest_len:
+            if get_remot(comb) < smallest_sums:
+                smallest_sums = get_remot(comb)
+                best_sol = comb.copy()
+    return best_sol
+
+
+def one_by_one_final(lst, maxsum, splitting, alg_type):
+    if splitting:
+        if alg_type == "fast":
+            return one_by_one_with_splitting(lst, maxsum)
+        elif alg_type == "best":
+            return best_one_by_one_with_splitting(lst, maxsum)
+    else:
+        if alg_type == "fast":
+            return one_by_one_solution(lst, maxsum)
+        elif alg_type == "best":
+            return best_one_by_one_solution(lst, maxsum)
+
+
 @callback(
     Output("group-creator-output", 'children'),
     Input("group-creator-calculate-btn", 'n_clicks'),
@@ -221,7 +373,8 @@ def update_output_div(n_clicks, lst_type_a, lst_type_b, lst_type_c, lst_type_d, 
     start = time()
     # A
     if len(lst_type_a) > 0:
-        solution = final_algo(lst_type_a, group_max, max_len, alg_type)
+        # solution = final_algo(lst_type_a, group_max, max_len, alg_type)
+        solution = one_by_one_final(lst_type_a, group_max, False, alg_type)
         if solution is None:
             out.append(dmc.Col(dmc.Text("Type A solution does not exist"), span="content"))
         else:
@@ -236,7 +389,8 @@ def update_output_div(n_clicks, lst_type_a, lst_type_b, lst_type_c, lst_type_d, 
             out.append(dmc.Col(inout, span="content"))
     # B
     if len(lst_type_b) > 0:
-        solution = final_algo(lst_type_b, group_max, max_len, alg_type)
+        # solution = final_algo(lst_type_b, group_max, max_len, alg_type)
+        solution = one_by_one_final(lst_type_b, group_max, False, alg_type)
         if solution is None:
             out.append(dmc.Col(dmc.Text("Type B solution does not exist"), span="content"))
         else:
@@ -251,7 +405,8 @@ def update_output_div(n_clicks, lst_type_a, lst_type_b, lst_type_c, lst_type_d, 
             out.append(dmc.Col(inout, span="content"))
     # C, D, E
     if len(lst_type_c) + len(lst_type_d) + len(lst_type_e) > 0:
-        solution = final_algo_with_splitting(lst_type_c+lst_type_d+lst_type_e, group_max, max_len, alg_type)
+        # solution = final_algo_with_splitting(lst_type_c+lst_type_d+lst_type_e, group_max, max_len, alg_type)
+        solution = one_by_one_final(lst_type_c+lst_type_d+lst_type_e, group_max-20, True, alg_type)
         if solution is None:
             out.append(dmc.Col(dmc.Text("Type C,D,E solution does not exist"), span="content"))
         else:
